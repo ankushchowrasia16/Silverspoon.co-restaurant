680ced8d87bbef69ac39310c487b6d763b7590d0
ankushchowrasia16 <ravisharma04272002@gmail.com>
2025-12-03 23:45:56 +0530

    Create DishDetailPage component for individual dish detail pages.

---
+++ src/pages/DishDetailPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DishDetailPage = () => {
+    return (
+        <div>
+            <h1>Dish Detail Page</h1>
+            {/* Add details about the dish here */}
+        </div>
+    );
+};
+
+export default DishDetailPage;
